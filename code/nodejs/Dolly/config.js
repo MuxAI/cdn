bccf194f61a5fcf92b4aa5ac9f5159245e6c98bd
Dewan Mukto <dewanmukto@gmail.com>
2025-07-31 15:24:01 +0600

    Re-upload here from main

---
+++ code/nodejs/Dolly/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    TOKEN: "",
+    PREFIX: "",
+    LEAVE_IDLE: true,
+    LEAVE_IDLE_COUNTDOWN: 60,
+    LEAVE_FINISH: false,
+    LEAVE_STOP: false,
+    WEBHOOK_ID: 980158201297248257,
+    WEBHOOK_TOKEN: "",
+}
